2d3575aac70f8e86ea417980c6847bdf15921261
ianalves725 <naiomagician725@gmail.com>
2022-09-25 15:08:03 -0400

    Updated JS

---
+++ static/index.js
@@ -0,0 +1,11 @@
+function validation(){
+    var username = document.getElementById("username");
+    var username_error = document.getElementById("error_username");
+
+    if (username.value.length <= 5){
+        username_error.innerHTML = "username is too short.";
+        username_error.style.color = "#ff0000";
+
+        return false;
+    }
+}
